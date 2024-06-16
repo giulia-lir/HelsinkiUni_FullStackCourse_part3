b020eef40314f824c5aa3154d7561ead9b17f619
giulia-lir <giulia.libe3@gmail.com>
2024-06-16 20:24:33 +0300

    phonebook part 3.12

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://GiuliaLibe:${password}@cluster0.qgree8v.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: personName, 
+  number: personNumber
+})
+
+const addPerson = () => {
+  person.save().then(result => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+const listAll = () => {
+  console.log("phonebook: ")
+  Person.find({}).then(result => {
+    result.forEach(person => {
+        console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+
+(process.argv.length === 5) ? addPerson() : listAll()
